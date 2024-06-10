f0829d7444f39ba33e4ab4b47d6a6a8645823edf
maker-ymc <young8@knou.ac.kr>
2024-06-11 08:44:58 +0900

    api호출객체 완료. 필수값이 0을 초과하는 음식만 입력

--- src/main/java/com/sds/foodadmin/model/food/FoodDBApiService.java
+++ src/main/java/com/sds/foodadmin/model/food/FoodDBApiService.java
@@ -38,45 +38,61 @@ public class FoodDBApiService implements FoodDBService {
 
 	public String getData() {
 		jsonString = "";
+		int startIndex = 1;
+		int endIndex = 999;
+		int callSize = 999;
 
 		log.debug("key 날라옴?=========" + key);
 		String urlFrame = "https://openapi.foodsafetykorea.go.kr/api/";
 
 		try {
+			boolean dataCall = true;
+
 			StringBuilder stringBuilder = new StringBuilder(urlFrame); // URL헤드
 			stringBuilder.append(key); // 키값 : key 못불러와서 수동맵핑. 나중에 수정
-			stringBuilder.append("/I2790/json/1"); // 서비스코드 + 객체형테 + 1번째 콘텐츠 호출
-			stringBuilder.append("/999"); // 1번 호출 시 999개 호출할 수 있음 (변수)
-			URL url = new URL(stringBuilder.toString());
-			log.debug("조립한 url은" + url);
 
-			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-			conn.setRequestMethod("GET");
-			conn.setRequestProperty("Content-type", "application/json");
-			log.debug("Response code: " + conn.getResponseCode());
+			while (dataCall) {
+				stringBuilder.append("/I2790/json/" + startIndex); // 서비스코드 + 객체형테 + startIndex
+				stringBuilder.append("/" + endIndex); // 1번 호출 시 999개 호출할 수 있음 (변수)
+				URL url = new URL(stringBuilder.toString());
+				log.debug("조립한 url은" + url);
 
-			BufferedReader rd;
-			if (conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
-				rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-			} else {
-				rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+				HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+				conn.setRequestMethod("GET");
+				conn.setRequestProperty("Content-type", "application/json");
+				log.debug("Response code: " + conn.getResponseCode());
+
+				BufferedReader rd;
+				if (conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
+					log.debug("200 이상 300이하에서 응답함");
+					rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+				} else {
+					rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+				}
+				StringBuilder sb = new StringBuilder();
+				String line;
+				while ((line = rd.readLine()) != null) {
+					sb.append(line);
+				}
+				String tempJson = sb.toString(); // 임시변수
+				log.debug("추출했음?=========" + tempJson); // 가져온 데이터를 출력
+				JsonObject jsonObject = JsonParser.parseString(tempJson).getAsJsonObject();
+
+				// 앞에 헤드 값과 result 때문에 파싱 시 오류남. row의 값만 가져오도록 설정
+				JsonArray rowArray = jsonObject.getAsJsonObject("I2790").getAsJsonArray("row"); // 이 시점에서 row 안의 값만 가져옴
+				jsonString = rowArray.toString();
+
+				if (rowArray.size() == 0) {
+					// 더 이상 데이터가 없다면
+					dataCall = false;
+				} else {
+					startIndex = endIndex + 1;
+					endIndex += callSize;
+				}
+
+				rd.close();
+				conn.disconnect();
 			}
-			StringBuilder sb = new StringBuilder();
-			String line;
-			while ((line = rd.readLine()) != null) {
-				sb.append(line);
-			}
-			jsonString = sb.toString();
-
-			log.debug("추출했음?=========" + jsonString); // 들어왔는지 확인
-
-			// 앞에 헤드 값과 result 때문에 파싱 시 오류남. row의 값만 가져오도록 설정
-			JsonObject jsonObject = JsonParser.parseString(jsonString).getAsJsonObject();
-			JsonArray rowArray = jsonObject.getAsJsonObject("I2790").getAsJsonArray("row");
-			jsonString = rowArray.toString(); // 이 시점에서 jsonString은 row 안의 값만 가져옴
-
-			rd.close();
-			conn.disconnect();
 
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -90,15 +106,13 @@ public class FoodDBApiService implements FoodDBService {
 
 		log.debug("create 메서드에 string 넘어옴?===" + jsonString);
 
-		// String 을 FoodDB 행렬로 담을 객체 미리 만듦
+		// String을 FoodDB 리스트로 담을 객체 미리 만듦
 		List<FoodDB> foodDBList = new ArrayList<>();
 
 		try {
-			//
 			JsonNode jsonArray = objectMapper.readTree(jsonString);
 
 			if (jsonArray.isArray()) {
-
 				for (JsonNode jsonObject : jsonArray) {
 
 					// 각 필드에 대한 데이터 확인 후 파싱
@@ -108,33 +122,21 @@ public class FoodDBApiService implements FoodDBService {
 
 						String foodname = jsonObject.get("DESC_KOR").asText(); // 음식명
 
-						// Node에 2차 파싱해서 Null값을 체크해준다
-						// null이 있으면 결과 객체에 대한 신뢰도가 떨어짐
-						// 필수값을 모두 만족하는 객체만을 입력하도록 하자
-						JsonNode nutrNode1 = jsonObject.get("NUTR_CONT1");
-						JsonNode nutrNode2 = jsonObject.get("NUTR_CONT2");
-						JsonNode nutrNode3 = jsonObject.get("NUTR_CONT3");
-						JsonNode nutrNode4 = jsonObject.get("NUTR_CONT4");
-						JsonNode nutrNode5 = jsonObject.get("NUTR_CONT5");
-						JsonNode nutrNode6 = jsonObject.get("NUTR_CONT6");
+						// 각 필드 파싱
+						Float kcal = parseFloatValue(jsonObject.get("NUTR_CONT1"));
+						Float carbohydrate = parseFloatValue(jsonObject.get("NUTR_CONT2"));
+						Float protein = parseFloatValue(jsonObject.get("NUTR_CONT3"));
+						Float fat = parseFloatValue(jsonObject.get("NUTR_CONT4"));
+						Float sugar = parseFloatValue(jsonObject.get("NUTR_CONT5"));
+						Float sodium = parseFloatValue(jsonObject.get("NUTR_CONT6"));
 
-						if ((nutrNode1.isNull() || nutrNode1.asInt() == 0)
-								&& (nutrNode2.isNull() || nutrNode2.asInt() == 0)
-								&& (nutrNode3.isNull() || nutrNode3.asInt() == 0)
-								&& (nutrNode4.isNull() || nutrNode4.asInt() == 0)
-								&& (nutrNode5.isNull() || nutrNode5.asInt() == 0)
-								&& (nutrNode6.isNull() || nutrNode6.asInt() == 0)) {
-							log.debug("필수값 중 null 또는 0이 존재해 객체를 건너뜁니다");
+						// 0보다 작거나 같은 값, null이 있는지 확인
+						if (kcal == null || carbohydrate == null || protein == null || fat == null || sugar == null
+								|| sodium == null || kcal <= 0.0f || carbohydrate <= 0.0f || protein <= 0.0f
+								|| fat <= 0.0f || sugar <= 0.0f || sodium <= 0.0f) {
+							log.debug("필수값 중 0보다 작거나 같은 값 또는 null이 존재해 객체를 건너뜁니다");
 							continue;
 						}
-
-						// 각 필드 파싱
-						float kcal = (float) nutrNode1.asDouble();
-						float carbohydrate = (float) nutrNode2.asDouble();
-						float protein = (float) nutrNode3.asDouble();
-						float fat = (float) nutrNode4.asDouble();
-						float sugar = (float) nutrNode5.asDouble();
-						float sodium = (float) nutrNode6.asDouble();
 
 						// FoodDB 객체 생성
 						FoodDB foodDB = new FoodDB();
@@ -158,6 +160,13 @@ public class FoodDBApiService implements FoodDBService {
 			e.printStackTrace();
 		}
 		return foodDBList;
+	}
+
+	private Float parseFloatValue(JsonNode node) {
+		if (node.isNull() || node.asText().isEmpty()) {
+			return null; // 빈 문자열 또는 null인 경우 null 반환
+		}
+		return (float) node.asDouble();
 	}
 
 	@Override
@@ -184,8 +193,3 @@ public class FoodDBApiService implements FoodDBService {
 	}
 
 }
-
-/*
- * restTemplate방식(백업용)
- * 
- */
